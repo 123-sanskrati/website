a8f0293be5e55f5bee190e2c8df7b3918ff8c862
123-sanskrati <74099905+123-sanskrati@users.noreply.github.com>
2025-02-08 10:13:08 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -26,7 +26,7 @@ function Signup() {
       return handleError("name ,email are required");
     }
     try {
-      const url = `http://localhost:8080/auth/signup`;
+      const url = `https://userloginportalapi.vercel.app/auth/signup`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
